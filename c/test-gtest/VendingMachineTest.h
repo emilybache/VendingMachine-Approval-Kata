c47403e4147c708552bdb9a178091cf610529946
emily <emily@bacheconsulting.com>
2021-04-23 12:58:11 +0200

    first test passing

--- c/test-gtest/VendingMachineTest.h
+++ c/test-gtest/VendingMachineTest.h
@@ -5,33 +5,12 @@
 
 #include "vending_machine.h"
 
-typedef struct coin {
-   char name[10];
-   int value;
-} coin;
-
 class VendingMachineTest : public ::testing::Test {
 public:
     struct vending_machine* machine;
 
     void SetUp() override {
         machine = vending_machine_create();
-    }
-
-    int coin_with_name(std::string name) {
-        if (name == "penny") {
-            return 1;
-        }
-        if (name == "nickel") {
-            return 5;
-        }
-        if (name == "dime") {
-            return 10;
-        }
-        if (name == "quarter") {
-            return 25;
-        }
-        return 0;
     }
 
     void TearDown() override {
