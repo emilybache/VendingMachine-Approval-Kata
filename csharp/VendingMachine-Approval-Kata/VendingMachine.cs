9385382088f49ce0c65bbf2ad5b33ee15580ad4c
neppord <neppord@gmail.com>
2022-09-22 20:43:28 +0200

    Copy python implementation to csharp

--- csharp/VendingMachine-Approval-Kata/VendingMachine.cs
+++ csharp/VendingMachine-Approval-Kata/VendingMachine.cs
@@ -2,33 +2,38 @@
 
 public class VendingMachine
 {
-    private readonly List<int> _coins;
-    private readonly List<int> _returns;
+    private readonly List<int> _acceptedCoins = new () {5, 10 ,25};
+    private List<int> _bank = new();
+    private List<int> _coins = new();
+    public string? DispensedProduct = "";
+    private Dictionary<string, int> _prices = new()
+    {
+        { "Cola", 100 }, { "Chips", 50 }, { "Candy", 65 }
+    };
+    private List<int> _returns = new ();
+    private string? _selectedProduct;
+    private Dictionary<string, int> _stock;
+
+    public VendingMachine(string selectedProduct, Dictionary<string, int> stock)
+    {
+        Display = "";
+        Balance = 0;
+        _stock = stock;
+        _selectedProduct = selectedProduct;
+        DisplayBalance();
+    }
+
     public string Display { get; private set; }
     public int Balance { get; private set; }
-    private readonly int[] _acceptedCoins;
 
-    public VendingMachine():this("", new List<int>(), new List<int>(), 0, new int[]{5, 10, 25})
+    private void DisplayBalance()
     {
-    }
-
-    public VendingMachine(String display, List<int> coins, List<int> returns, int balance, int[] acceptedCoins) {
-        Display = display;
-        _coins = coins;
-        _returns = returns;
-        Balance = balance;
-        _acceptedCoins = acceptedCoins;
-
-        displayBalance();
-    }
-
-    private void displayBalance() {
-        if(Balance != 0){
+        if (Balance != 0)
             Display = "" + Balance;
-        }
-        else{
+        else if (HasChange())
             Display = "INSERT COIN";
-        }
+        else
+            Display = "EXACT CHANGE ONLY";
     }
 
     public int[] Coins()
@@ -36,14 +41,108 @@ public class VendingMachine
         return _coins.ToArray();
     }
 
-    public void InsertCoin(int coin){
-        if(_acceptedCoins.Contains(coin)){
+    public void InsertCoin(int coin)
+    {
+        if (_acceptedCoins.Contains(coin))
+        {
             _coins.Add(coin);
             Balance += coin;
-            displayBalance();
+            DisplayBalance();
         }
-        else{
+        else
+        {
             _returns.Add(coin);
         }
+    }
+
+    public void SelectProduct(string product)
+    {
+        _selectedProduct = product;
+        if (Balance >= _prices[_selectedProduct])
+        {
+            DispenseProduct();
+        }
+        else
+        {
+            var cost = _prices[product];
+            var cent = Convert.ToDecimal(cost) / 100;
+            Display = $"PRICE {cent:#.00}";
+        }
+    }
+
+    private void DispenseProduct()
+    {
+        if (_selectedProduct != null && _stock[_selectedProduct] >= 1)
+        {
+            _stock[_selectedProduct] -= 1;
+            Display = "THANK YOU";
+            DispensedProduct = _selectedProduct;
+            Balance -= _prices[_selectedProduct];
+            _bank.Append<>(_coins);
+            _coins = new List<int>();
+            if (Balance > 0)
+            {
+                var change = GetChangeRequired(Balance);
+                _returns = change;
+                foreach (var coin in change) _bank.Remove(coin);
+                Balance = 0;
+            }
+
+            _selectedProduct = null;
+        }
+        else
+        {
+            Display = "SOLD OUT";
+            _selectedProduct = null;
+        }
+    }
+
+    public void Tick()
+    {
+        if (Display.Contains("PRICE"))
+        {
+            DisplayBalance();
+        }
+        else if (
+            Display.Contains("THANK YOU") ||
+            Display.Contains("SOLD OUT")
+        )
+        {
+            DisplayBalance();
+            DispensedProduct = null;
+            _returns = new List<int>();
+        }
+        else if (_selectedProduct != null && Balance >= _prices[_selectedProduct])
+        {
+            DispenseProduct();
+        }
+    }
+
+    public void UpdateStock(string product, int quantity)
+    {
+        _stock[product] = quantity;
+    }
+
+    public void ReturnCoins()
+    {
+        Balance = 0;
+        _returns = _coins;
+        _coins = new List<int>();
+        DisplayBalance();
+    }
+
+    private bool HasChange()
+    {
+        return
+            _bank.Contains(25)
+            && _bank.Contains(10)
+            && _bank.Contains(5)
+            ;
+    }
+
+    private List<int> GetChangeRequired(int balance)
+    {
+        // TODO: don't fake this one!
+        return new List<int> { 10, 5 };
     }
 }
